2949acb1d75040ccdf13aa28dbc2102c7541f292
Jacob Hampton <jhampton792@gmail.com>
2021-06-14 12:10:48 -0500

    Updated button to show alert when clicked

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,10 +1,14 @@
 const LaunchItem = ({ launch }) => {
 
+  const handleClick = (launch) => {
+    alert(`The user selected details for ${ launch.name }`);
+  }
+
   return(
     <div className={`launch-card ${ launch.success ? "successful-launch " : "failed-launch" }`}>
       <p>Name: { launch.name }</p>
       <p>Date: { launch.date_utc }</p>
-      <button className="launch-details-btn">Show Details</button>
+      <button onClick={ () => { handleClick(launch) } } className="launch-details-btn">Show Details</button>
     </div>
   )
 
